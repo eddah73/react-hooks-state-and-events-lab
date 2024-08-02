1aff80b0520e1d2c8a0ed1c3371b64a5b04901c4
eddah73 <eddahsjr@gmail.com>
2024-08-02 14:27:23 +0300

    states in array

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React ,{useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false)
+  //add to cart
+  function addToCart(){
+    setIsInCart(true)
+  }
+  //remove from cart
+  function removeFromCart(){
+    setIsInCart(false)
+  }
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={isInCart ? removeFromCart : addToCart}>{isInCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
